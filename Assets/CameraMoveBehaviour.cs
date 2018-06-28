22ebfbf755eb5e5f5b5939843e6c7e89b7e5e725
Clement-R <rondeau.clement@gmail.com>
2018-06-28 08:49:46 +0200

    Start to work on camera movement

---
+++ Assets/CameraMoveBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveBehaviour : MonoBehaviour {
+
+    public Transform min;
+    public Transform max;
+
+    public float scrollSpeed = 10f;
+
+    private float _cameraHalfWidth;
+
+    private void Start()
+    {
+        _cameraHalfWidth = Camera.main.orthographicSize * ((float)Screen.width / Screen.height);
+    }
+
+    void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Move(-1f);
+        } else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Move(1f);
+        }
+    }
+
+    private void Move(float direction)
+    {
+        transform.Translate(new Vector3(scrollSpeed * direction, 0f, 0f));
+
+        
+    }
+}
