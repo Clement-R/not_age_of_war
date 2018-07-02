088b97ef351b9f33ed7b9b7ebe2e0b004a41d617
Clement-R <rondeau.clement@gmail.com>
2018-07-02 13:37:41 +0200

    Clean code

--- Assets/Scripts/GoldDropEffect.cs
+++ Assets/Scripts/GoldDropEffect.cs
@@ -32,18 +32,10 @@ public class GoldDropEffect : MonoBehaviour
 
 	private void OnCollisionEnter2D(Collision2D other)
 	{
-		Debug.Log("Collision with floor");
-
 		moneyText = GameData.Instance.moneyText;
-
-		// TODO : Got to money transform
-		// Vector3 target = Camera.main.ScreenToWorldPoint(moneyText.rectTransform.position);
-		
-		Debug.Log((moneyText.transform.position - transform.position).normalized);
 
 		_rb2d.gravityScale = 0f;
 		_trail.enabled = true;
-		// _rb2d.AddForce((moneyText.transform.position - transform.position).normalized * 1000f, ForceMode2D.Impulse);
 		goTo = true;
 	}
 
@@ -58,7 +50,6 @@ public class GoldDropEffect : MonoBehaviour
 	{
 		// Give money to enemy
 		pkm.EventManager.EventManager.TriggerEvent("UnitDie", new { side = Side.RIGHT, reward = reward });
-		
 		Destroy(this.gameObject);
 	}
 }
